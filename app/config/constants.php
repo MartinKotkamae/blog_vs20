1ec9cdbc4d37b224afee6388d1cbacdee2bfb150
Martin <martin.kotkamae@khk.ee>
2021-01-25 12:37:20 +0200

    Määratud raamistiku konstandid ja projekti parameetrid

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 // Project name
-define("PROJECT_NAME","prokjectname");
+define("PROJECT_NAME","blog_vs20");
 // App root dir
 define("APPROOT", dirname(dirname(__FILE__)));
 // URL root
@@ -8,7 +8,7 @@ define("URLROOT", "http://".$_SERVER["HTTP_HOST"]."/".PROJECT_NAME);
 
 
 // db params
-define("DB_HOST", "host");
-define("DB_USER", "user");
-define("DB_PASS", "pass");
-define("DB_NAME", "dbname");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "blog_vs20");
